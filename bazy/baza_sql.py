491ca55e3998d0a7c45b018b42b4e7cbd5f4db75
kl3ag1 <kl3ag1@komp11.lo1cg.org>
2017-10-10 12:32:14 +0200

    sqlite3 baza

--- bazy/baza_sql.py
+++ bazy/baza_sql.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 #
 #  baza_sql.py
+
 import sqlite3
+from dane import * 
 
 
 def main(args):
@@ -13,7 +15,21 @@ def main(args):
     with open('pracownicy_z1.sql', 'r') as plik:
         skrypt = plik.read()
         cur.executescript(skrypt)
+        
+    pracownicy = dane_z_pliku('pracownicy.txt')
+    pracownicy = wyczysc_dane(pracownicy, 5)
+        
+    premia = dane_z_pliku('premia.txt')
+    premia = wyczysc_dane(premia, 1)
     
+    dzial = dane_z_pliku('dział.txt')
+    
+    print(pracownicy[0])
+    cur.executemany('INSERT INTO premia VALUES (?, ?)', premia)
+    cur.executemany('INSERT INTO dzial VALUES (?, ?, ?)', dzial)
+    cur.executemany('INSERT INTO pracownicy (id, nazwisko, imie, stanowisko, data_zatrudnienia, placa, id_dzial) VALUES (?, ?, ?, ?, ?, ?, ?)', pracownicy)
+    
+    con.commit() #zatwierdzenie operacji na bazie
     return 0
 
 if __name__ == '__main__':
