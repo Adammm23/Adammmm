9f86b15e77fd3133a6cd759a534e65d0b12e8959
Adammm23 <adam55333@wp.pl>
2018-01-29 20:56:00 +0100

    Add files via upload
    pd

---
+++ kody/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "uczen.h"
+
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    Uczen u1();
+    cout << "Średnia ucznia: ";
+    u1.srednia();
+        
+	return 0;
+}
+
